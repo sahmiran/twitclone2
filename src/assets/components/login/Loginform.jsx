1ee3a39b801a0b7683483bafc3b3f57e0e076eae
Hasan Can TURAL <hasancantural19@gmail.com>
2024-06-27 17:05:23 +0300

    form css oynaştık

--- src/assets/components/login/Loginform.jsx
+++ src/assets/components/login/Loginform.jsx
@@ -2,7 +2,9 @@ import React, { useState } from "react";
 import "./Loginform.css";
 import CloseIcon from "@mui/icons-material/Close";
 import { Button } from "@mui/material";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import GoogleIcon from "@mui/icons-material/Google";
+import AppleIcon from "@mui/icons-material/Apple";
 
 function Loginform({ setLogin }) {
   const [username, setUsername] = useState("");
@@ -33,9 +35,23 @@ function Loginform({ setLogin }) {
             <div className="titletext">
               <h2>X'e giriş yap</h2>
             </div>
+
             <div className="inputAlanı">
-              {" "}
+              <Button className="beyazButon">
+                <span>
+                  <GoogleIcon className="gIcon" />{" "}
+                </span>
+                <p> Google ile kaydol </p>
+              </Button>
+              <Button className="beyazButon">
+                <span>
+                  <AppleIcon />
+                </span>
+                <p>Apple ile kaydol</p>
+              </Button>
+              <div className="boslukAlani"></div>
               <input
+                className="inputBox"
                 type="text"
                 placeholder="Display Name"
                 value={displayName}
@@ -43,15 +59,23 @@ function Loginform({ setLogin }) {
                 required
               />
               <input
+                className="inputBox"
                 type="text"
                 placeholder="Username"
                 value={username}
                 onChange={(e) => setUsername(e.target.value)}
                 required
               />
-              <Button type="submit">İleri</Button>
-              <Button>Şifreni mi unuttun?</Button>
-              <p>Henüz bir hesabın yok mu?</p>
+              <Button className="beyazButon" type="submit">
+                İleri
+              </Button>
+              <Button className="siyahButon">Şifreni mi unuttun?</Button>
+              <p>
+                Henüz bir hesabın yok mu?{" "}
+                <span>
+                  <Link>Kaydol</Link>
+                </span>
+              </p>
             </div>
           </div>
         </form>
